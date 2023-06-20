f631ef0d211d81da2d1dd4872ce885cf55da9940
gianca-03 <gianca.ruiz@hotmail.com>
2023-06-20 02:12:36 -0600

    Metodos de convertir categoryModel

--- Northwind/BackEnd/Controllers/CategoryController.cs
+++ Northwind/BackEnd/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using DAL.Implementations;
+﻿using BackEnd.Models;
+using DAL.Implementations;
 using DAL.Interfaces;
 using Entities.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +13,26 @@ namespace BackEnd.Controllers
     public class CategoryController : ControllerBase
     {
         private ICategoryDAL categoryDal;
+
+        private CategoryModel Convertir(Category category)
+        {
+            return (new CategoryModel
+            {
+                CategoryId = category.CategoryId,
+                CategoryName = category.CategoryName,
+                Description = category.Description
+            });
+        }
+
+        private Category Convertir(CategoryModel category)
+        {
+            return (new Category
+            {
+                CategoryId = category.CategoryId,
+                CategoryName = category.CategoryName,
+                Description = category.Description
+            });
+        }
 
         #region Constructores
         public CategoryController()
@@ -28,8 +49,14 @@ namespace BackEnd.Controllers
         public JsonResult Get()
         {
             IEnumerable<Category> categories = categoryDal.GetAll();
+            List<CategoryModel> models = new List<CategoryModel>();
 
-            return new JsonResult(categories);
+            foreach (var category in categories)
+            {
+                models.Add(Convertir(category));
+            }
+
+            return new JsonResult(models);
         }
 
         // GET api/<CategoryController>/5
@@ -38,7 +65,7 @@ namespace BackEnd.Controllers
         {
             Category category = categoryDal.Get(id);
 
-            return new JsonResult(category);
+            return new JsonResult(Convertir(category));
         }
 
         #endregion
--- Northwind/BackEnd/Models/CategoryModel.cs
+++ Northwind/BackEnd/Models/CategoryModel.cs
@@ -2,5 +2,8 @@
 {
     public class CategoryModel
     {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string? Description { get; set; }
     }
 }
