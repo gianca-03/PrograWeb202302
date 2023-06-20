5fbc675e151ef92676f5fbccbf22a2151886b2cc
gianca-03 <gianca.ruiz@hotmail.com>
2023-06-20 04:34:36 -0600

    Shipper helper controller view  getall & getById shippers button

---
+++ Northwind/FrontEnd/Helpers/ShipperHelper.cs
@@ -0,0 +1,47 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class ShipperHelper
+    {
+        ServiceRepository repository;
+
+        public ShipperHelper()
+        {
+            repository = new ServiceRepository();
+        }
+
+        public List<ShipperViewModel> GetAll()
+        {
+
+            List<ShipperViewModel> lista = new List<ShipperViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Shipper/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<ShipperViewModel>>(content);
+            }
+
+            return lista;
+        }
+
+        /// <summary>
+        /// Get category By Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ShipperViewModel GetById(int id)
+        {
+            ShipperViewModel shipper = new ShipperViewModel();
+
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Shipper/" + id);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+
+            shipper = JsonConvert.DeserializeObject<ShipperViewModel>(content);
+
+            return shipper;
+        }
+
+    }
+}
